fa09bc7fe6516068bc0a1d8d75612b4ee6513276
GenykGLI <131983720+GenykGLI@users.noreply.github.com>
2023-06-09 15:00:06 +0300

    Last changes

---
+++ hw_1.py
@@ -0,0 +1,30 @@
+# Exercise 1
+#
+# # t123 = int(input('Enter three-digit number: '))
+# #
+# # summa_of_numbers = (t123 // 100) * (t123 // 10 % 10) * (t123 % 10)
+# # multiplying_a_numbers = (t123 // 100) * (t123 // 10 % 10) * (t123 % 10)
+# #
+# # print(f'Sum of numbers: {summa_of_numbers}')
+# # print(f'Multiplying of numbers: {multiplying_a_numbers}')
+#
+# ##############################################################################################
+#
+# Exercise 2
+#
+# a_diagonal = int(input('Enter diagonal A: '))
+# b_diagonal = int(input('Enter diagonal B: '))
+#
+# print(f'The sum of diagonals = {a_diagonal + b_diagonal}')
+#
+# ##############################################################################################
+#
+# Exercise 3
+#
+# # t1234 = int(input('Enter a four-digit number: '))
+# #
+# # thousands, left = divmod(t1234, 1000)
+# # hundreds, left = divmod(left, 100)
+# # tens, left = divmod(left, 10)
+# #
+# # print(f'Multiplying of numbers: {left * thousands * hundreds * tens}')
---
+++ hw_4.py
@@ -0,0 +1,67 @@
+# Exercise 1
+
+# digit = 0
+# alphas = 0
+#
+# text = input('Enter you string: ')
+# for i in text:
+#     if i.isdigit():
+#         digit += 1
+#     elif i.isalpha():
+#         alphas += 1
+# print(f'Count of digits is {digit} \nCount of alphas is {alphas}')
+
+# ==========================================================================================
+
+# Exercise 2
+#
+# text = (input('Enter the string: '))
+# symbol = (input('Enter the symbol for search: '))
+#
+# s = text.count(symbol)
+# print('Count of symbols is', s)
+
+# ==========================================================================================
+
+# Exercise 3
+
+# text, word = input('Enter your text: '), input('Enter the word to find: ')
+# word_change = input('Enter the word to be replaced: ')
+#
+# text = text.replace(word, word_change)
+# print(text)
+
+# ==========================================================================================
+
+# Exercise 4
+
+# text = str(input('Enter the string: '))
+# print(text[3])
+# print(text[-2])
+# print(text[0:5])
+# print(text[0:-2])
+# print(text[::2])
+# print(text[1::2])
+# print(text[::-1])
+# print(text[::-2])
+# print(len(text))
+
+# ==========================================================================================
+
+# Additionally Exercise
+
+# word = (input('Enter the text: '))
+# first = word.split('. ')
+# second = 0
+#
+# for i in range(len(first)):
+#     first[i] = first[i].capitalize()
+# print('. '.join(first))
+# for i in word:
+#     if i.isdigit():
+#         second += 1
+# print(f'Count of digits is {second}')
+# print('Number of punctuation marks is', word.count(','))
+# print('Number of exclamation marks is', word.count('!'))
+
+# ==========================================================================================

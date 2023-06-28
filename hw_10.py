b61e8f9ccd822f949e049d384e2f35fba3c5c98d
GenykGLI <131983720+GenykGLI@users.noreply.github.com>
2023-06-28 09:25:48 +0300

    complete task hw_10 ex4 additional task

--- hw_10.py
+++ hw_10.py
@@ -37,14 +37,29 @@ import re
 # Exercise 3
 
 
-def validate_email(email):
-    result = re.search(r'^\w+@\w+\.\w{2,4}$', email)
-    print(result)
+# def validate_email(email):
+#     result = re.search(r'^\w+@\w+.\w{2,4}$', email)
+#     print(result)
+#
+#
+# def user_validate_email():
+#     user_input = input('Enter your email: ')  # For example   example@gmail.com
+#     validate_email(user_input)
+#
+#
+# user_validate_email()
+
+##################################################################################################
+
+# Exercise 4 additional task
+
+def validate_password(psd):
+    result = r'^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*\W).{8,16}$'
+    return re.search(result, psd)
 
 
-def user_validate_email():
-    user_input = input('Enter your email: ')  # For example   example@gmail.com
-    validate_email(user_input)
-
-
-user_validate_email()
+password = input('Enter your password: ')  # For example Qwerty1234@
+if validate_password(password):
+    print('Password syntax correct')
+else:
+    print('Incorrect syntax password')

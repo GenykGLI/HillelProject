0b6cd37ae3e791d2332bbb6375a2453277e97996
GenykGLI <131983720+GenykGLI@users.noreply.github.com>
2023-07-02 16:22:23 +0300

    !

--- hw_8.py
+++ hw_8.py
@@ -1,12 +1,14 @@
+import random
+
 # Exercise 1
 
-def degree_of_number(number):
-    if number <= 1:
-        return 1
-    return number * number + degree_of_number(number - 1)
-
-
-print(f'The sum of number degree in recursion is', degree_of_number(6))
+# def degree_of_number(number):
+#     if number <= 1:
+#         return 1
+#     return number * number + degree_of_number(number - 1)
+#
+#
+# print(f'The sum of number degree in recursion is', degree_of_number(6))
 
 
 ################################################################
@@ -33,20 +35,27 @@ print(f'The sum of number degree in recursion is', degree_of_number(6))
 # Exercise 3
 
 
-# def sum_of_digits(range_a, range_b, count=0):
-#     if range_a > range_b:
-#         return count
-#     count += range_a
-#     return sum_of_digits(range_a + 1, range_b, count)
-#
-#
-# def range_numbers():
-#     range_a = int(input('Enter initial number: '))
-#     range_b = int(input('Enter end number: '))
-#     print(sum_of_digits(range_a, range_b))
-#
+def sum_of_digits(range_a, range_b, count=0):
+    if range_a > range_b:
+        return count
+    count += range_a
+    return sum_of_digits(range_a + 1, range_b, count)
+
+
+def range_numbers():
+    range_a = int(input('Enter initial number: '))
+    range_b = int(input('Enter end number: '))
+    print(sum_of_digits(range_a, range_b))
+
 #
 # range_numbers()
 
 ################################################################
 
+# Exercise 4
+
+# number = [random.randrange(1, 100) for i in range(100)]
+# print(number)
+#
+# for i in number:
+#     if i
--- test.py
+++ test.py
@@ -1,20 +1,27 @@
-s = [2, 3, 4, 5, 6, 7, 11, 12, 13, 14, 15, 16, 17, 45, 55, 59]
-lst = []
+# Exercise 4
+
+import re
 
 
-def find(a):
-    poz = 0
-    for i in range(1, a + 1):
-        if a % i == 0:
-            poz += 1
-    if poz == 2:
-        lst.append(a)
+def validate_password(psd):
+    result = r'^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*+]).{8,16}$'
+    return re.search(result, psd)
 
 
-def check():
-    for i in s:
-        find(i)
-    print(f'Count of prime numbers is ', len(lst))
+password = 'Qwerty1234@'
+if validate_password(password):
+    print('Confirm syntax password')
+else:
+    print('Incorrect syntax password')
 
-
-check()
+# def validate_password(pwd):
+#     pattern = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&+=!])' \
+#               r'(?=.*[a-zA-Z\d@#$%^&+=!]).{8,16}$'
+#     return re.match(pattern, pwd) is not None
+#
+#
+# password = "Abcd1234@"
+# if validate_password(password):
+#     print("Пароль валиден")
+# else:
+#     print("Пароль невалиден")
